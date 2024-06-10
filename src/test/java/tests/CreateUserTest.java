268bfc82462b8851cd0b8840bf8db14663c1c814
MainoddinS <ms.sayyad97@gmail.com>
2024-06-10 12:52:58 +0530

    Modified CreateUser method

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -1,5 +1,43 @@
 package tests;
 
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static org.hamcrest.Matchers.*;
+
+import org.apache.http.HttpStatus;
+
 public class CreateUserTest {
 
+	@Test
+	public void addNewUser() {
+//		RestAssured.baseURI = "https://gorest.co.in";
+		
+//		User user = new User("Naveen", getRandomEmailId(), "male", "active");
+
+		String jsonBody = "{" +
+                "   \"name\":\"Test Automation\",\n" +
+                "   \"email\":\"testAutomation303@gmail.com\",\n" +
+                "   \"gender\":\"Male\",\n" +
+                "   \"status\":\"active\"\n" +
+                "}";
+		
+		given().log().all()
+		.header("Authorization", "Bearer c3c7842cea56c6c93309e0d45365437d6e84771dc68fc97ed24a080c9bcd98f0")
+	    .accept(ContentType.JSON)
+	    .contentType(ContentType.JSON)
+	    .and().body(jsonBody)
+		
+		.when().log().all()
+		.post("https://gorest.co.in/public-api/users")
+		
+		.then()
+		.assertThat()
+		.statusCode(HttpStatus.SC_OK)
+		.and()
+		.body("data.name", equalTo("Test Automation"));
+	    
+	}
 }
--- src/test/java/tests/GetUserTest.java
+++ src/test/java/tests/GetUserTest.java
@@ -3,22 +3,21 @@ package tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import base.BaseTest;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-public class GetUserTest extends BaseTest{
+public class GetUserTest {
 
 	@Test
 	public void getAllUsers() {
-		RestAssured.baseURI = baseURI;
+		RestAssured.baseURI = "https://gorest.co.in";
 		Response response = given()
 				        .header("Authorization", "Bearer c3c7842cea56c6c93309e0d45365437d6e84771dc68fc97ed24a080c9bcd98f0")
 	                    .when()
-	                    .get(GOREST_ENDPOINT+"/"+2);
+	                    .get("/public/v2/users/3");
 	
 		JsonPath js = response.jsonPath();
 		System.out.println(js.getInt("id"));
